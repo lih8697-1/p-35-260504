f1da3f4f91982602ab539edaba8205db95ad9655
lih8697-1 <lih8697@gmail.com>
2026-05-06 12:00:37 +0900

    10-findQByUsername queryDsl로 구현

--- backend/src/test/kotlin/com/back/domain/member/repository/MemberRepositoryTest.kt
+++ backend/src/test/kotlin/com/back/domain/member/repository/MemberRepositoryTest.kt
@@ -1,16 +1,12 @@
 package com.back.domain.member.repository
 
 import com.back.global.extentions.getOrThrow
-import jakarta.transaction.Transactional
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles("test")
-@Transactional
 class MemberRepositoryTest {
 
     @Autowired
@@ -26,5 +22,17 @@ class MemberRepositoryTest {
     fun `findQById()`() {
         val member = memberRepository.findQById(1).getOrThrow()
         assertThat(member.id).isEqualTo(1)
+    }
+
+    @Test
+    fun `findByUsername()`() {
+        val member = memberRepository.findByUsername("user1").getOrThrow()
+        assertThat(member.username).isEqualTo("user1")
+    }
+
+    @Test
+    fun `findQByUsername()`() {
+        val member = memberRepository.findQByUsername("user1").getOrThrow()
+        assertThat(member.username).isEqualTo("user1")
     }
 }
