71e88afc064e77a7b9774f3d5f2e7b2486897b14
lih8697-1 <lih8697@gmail.com>
2026-05-07 11:13:46 +0900

    32-enum 도입

--- backend/src/main/kotlin/com/back/domain/member/repository/MemberRepositoryCustom.kt
+++ backend/src/main/kotlin/com/back/domain/member/repository/MemberRepositoryCustom.kt
@@ -1,6 +1,7 @@
 package com.back.domain.member.repository
 
 import com.back.domain.member.entity.Member
+import com.back.standard.enums.MemberSearchKeywordType
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
@@ -18,5 +19,5 @@ interface MemberRepositoryCustom {
     fun countQByNicknameContaining(nickname: String): Long
     fun existsQByNicknameContaining(nickname: String): Boolean
     fun findQByUsernameContaining(username: String, pageable: Pageable): Page<Member>
-    fun findByKwPaged(kw: String, kwType: String, pageable: Pageable): Page<Member>
+    fun findByKwPaged(kw: String, kwType: MemberSearchKeywordType, pageable: Pageable): Page<Member>
 }
