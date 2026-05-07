eb05caaddc6e2d74813a0bff4d22f7b9d71ca249
lih8697-1 <lih8697@gmail.com>
2026-05-07 10:16:22 +0900

    29-pageDto 도입하여 불필요한 응답 제거

--- backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
+++ backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
@@ -2,7 +2,7 @@ package com.back.domain.member.controller
 
 import com.back.domain.member.dto.MemberWithUsernameDto
 import com.back.domain.member.service.MemberService
-import org.springframework.data.domain.Page
+import com.back.standard.ut.dto.PageDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -18,15 +18,18 @@ class ApiV1AdmMemberController(
     fun list(
         @RequestParam("page", defaultValue = "1") page: Int,
         @RequestParam("pageSize", defaultValue = "2") pageSize: Int
-    ): Page<MemberWithUsernameDto> {
+    ): PageDto<MemberWithUsernameDto> {
 
         // 페이지 예외 처리
-        val page = if(page >= 1) page else 1
-        val pageSize = if(page >= 5) pageSize else 5
+        val page = if (page >= 1) page else 1
+        val pageSize = if (pageSize >= 5) pageSize else 5
 
-        val pagedResult  = memberService.findByPaged(page, pageSize)
+        val pagedResult = memberService.findByPaged(page, pageSize)
 
-        return pagedResult
-            .map { MemberWithUsernameDto(it) }
+        return PageDto(
+            pagedResult.map {
+                MemberWithUsernameDto(it)
+            }
+        )
     }
 }
