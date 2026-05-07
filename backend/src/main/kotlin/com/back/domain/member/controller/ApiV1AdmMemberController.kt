2f32f24e8329286f21a333c7103f956b9d6eb66b
lih8697-1 <lih8697@gmail.com>
2026-05-07 11:28:19 +0900

    33-getter 이용

--- backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
+++ backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
@@ -4,6 +4,7 @@ import com.back.domain.member.dto.MemberWithUsernameDto
 import com.back.domain.member.service.MemberService
 import com.back.standard.dto.PageDto
 import com.back.standard.enums.MemberSearchKeywordType
+import com.back.standard.enums.MemberSearchSortType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -21,13 +22,14 @@ class ApiV1AdmMemberController(
         @RequestParam("pageSize", defaultValue = "2") pageSize: Int,
         @RequestParam("kw", defaultValue = "") kw: String,
         @RequestParam("kwType", defaultValue = "ALL") kwType: MemberSearchKeywordType,
+        @RequestParam("sort", defaultValue = "ID") sort: MemberSearchSortType,
     ): PageDto<MemberWithUsernameDto> {
 
         // 페이지 예외 처리
         val page = if (page >= 1) page else 1
         val pageSize = if (pageSize >= 5) pageSize else 5
 
-        val pagedResult = memberService.findByPaged(page, pageSize, kw, kwType)
+        val pagedResult = memberService.findByPaged(page, pageSize, kw, kwType, sort)
 
         return PageDto(
             pagedResult.map {
